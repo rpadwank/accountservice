ca1df56c172ab93a12c1039a47acc4e6ab4282c8
Padwankar <ruchir-shailesh.padwankar@capgemini.com>
2019-05-08 16:11:30 +0530

    v2.0

--- src/main/java/com/capgemini/accountservice/controller/AccountController.java
+++ src/main/java/com/capgemini/accountservice/controller/AccountController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.capgemini.accountservice.entity.BankAccount;
 import com.capgemini.accountservice.service.BankAccountService;
 
+@CrossOrigin("*")
 @RestController
 public class AccountController {
 	@Autowired
